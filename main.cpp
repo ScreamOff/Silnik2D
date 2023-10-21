db06a621418297d1d6c82f5b6c9ad8c85ce5d9c9
Teriash <138162965+Teriash@users.noreply.github.com>
2023-10-21 22:56:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main() {
 
     Rectangle* rect = new Rectangle(100, 100, 200, 100);
     Circle* circle = new Circle(300, 300, 50);
+    Line* line = new Line(700, 200, 500, 500);
 
     engine.addPrimitive(rect);
     engine.addPrimitive(circle);
+    engine.addPrimitive(line);
 
     engine.loadBitmap("image.png");
 
